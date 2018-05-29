534c0aa159517e7a6de4fbdd75d6f7c674130148
Kody Quintana <Quintana.Kody@gmail.com>
2018-05-28 21:15:49 -0400

    Minor changes

--- main.py
+++ main.py
@@ -7,14 +7,25 @@ import configparser
 embed_list = []
 nested_list_sections = []
 
-
 config_file = 'sections_config.ini'
+#xelatex_path = 'xelatex'
+xelatex_path = input('Enter path to xelatex: ')
 #work_dir = './work'
 output_dir = './output'
 grab_dir = './tempemb'
 template_dir = './TeX'
 
 #change to contextlib later
+
+def resource_path(relative_path):
+    #Get absolute path to resource, works for dev and for PyInstaller
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
 
 with tempfile.TemporaryDirectory() as work_dir:
 
@@ -26,14 +37,14 @@ with tempfile.TemporaryDirectory() as work_dir:
     print('\n')
     
     
-    def GetFileList(ext,dir):
+    def get_file_list(ext,dir):
         list = []
         for i in os.listdir(dir):
             if i.endswith(ext):
                 list.append(i)
         return list
     
-    def PdfRename():
+    def pdf_rename():
         global nested_list_sections
         global embed_list
     
@@ -41,7 +52,7 @@ with tempfile.TemporaryDirectory() as work_dir:
             # -1 from actual length but configparser makes a DEFAULT section that is not used so the -1 is fine
             nested_list_sections.append([])
     
-        for i, k in enumerate((GetFileList('pdf',grab_dir))):
+        for i, k in enumerate((get_file_list('pdf',grab_dir))):
             if (' ' in k):
     
                 #Splits document shorthand, removes leading 0s so that 2.1 is same as 2.01
@@ -75,10 +86,9 @@ with tempfile.TemporaryDirectory() as work_dir:
     for i in embed_list:
         print(i)
     
-    def TemplateStage(src,dest):
+    def template_stage(src,dest):
         
-        if not os.path.exists(dest):
-            os.mkdir(dest)
+        folder_check(dest)
         print('\nWorking directory: ' + str(work_dir))
     
         shutil.copytree(template_dir + '/font/',dest + '/font/')
@@ -88,21 +98,18 @@ with tempfile.TemporaryDirectory() as work_dir:
                 shutil.copy(src + '/' + i,dest)
 
     
-    def EmbedStage(src,dest):
+    def embed_stage(src,dest):
     
-        if not os.path.exists(dest):
-            os.mkdir(dest)
-    
+        folder_check(dest) 
         for i in os.listdir(src):
             if i.endswith('.pdf'):
                 shutil.copy(src + '/' + i,dest)
-                print (i)
-        
+                print (i)    
     
     
-    def CompileTeX(path,texfile):
+    def compile_TeX(path,texfile):
         for _ in range(2):
-            p = subprocess.Popen([path, texfile], cwd=work_dir)
+            p = subprocess.Popen([path, '-recorder', texfile], cwd=work_dir)
             p.wait()
 
     
@@ -111,8 +118,8 @@ with tempfile.TemporaryDirectory() as work_dir:
             os.mkdir(folder)
 
    
-    def JobInfo():
-        data_file = ['mo = 1234567', 'serial = 12345', 'equipment = thing', 'customer = SomeCorp']
+    def job_info():
+        data_file = ['mo = 1234567', 'serial = 12345', 'equipment = RA-24 thing', 'customer = SomeCorp']
 
         with open(work_dir + '/jobinfo.dat', 'w') as job_info_file:
             for i in data_file:
@@ -120,20 +127,14 @@ with tempfile.TemporaryDirectory() as work_dir:
         job_info_file.close()
 
 
-    def OutputPDF(output_path):
-        if not os.path.exists(output_path):
-            os.mkdir(output_path)
+    def output_pdf(output_path):
+        folder_check(output_path)
         print('\nOutput directory: ' + str(output_path))
         shutil.copy(work_dir + '/databook.pdf', output_path)
 
 
-    TemplateStage(template_dir, work_dir)
-    PdfRename()
-    JobInfo()
-    CompileTeX('xelatex','databook') 
-    OutputPDF(output_dir)
-    #EmbedStage(grab_dir, work_dir)
-    #
-    #print(grab_dir,work_dir)
-    
-    
+    template_stage(template_dir, work_dir)
+    pdf_rename()
+    job_info()
+    compile_TeX(xelatex_path,'databook') 
+    output_pdf(output_dir)
