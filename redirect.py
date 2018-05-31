0912cfca16922777430144c1a236a26e0f5537af
Kody.Quintana <Quintana.Kody@gmail.com>
2018-05-31 10:59:48 -0400

    Changed QProcess names for the XeLaTeX renders. Reduced stdout functions to two. (not sure if good way of doing it because of weird python3 exec() functionality)

--- redirect.py
+++ redirect.py
@@ -1,4 +1,3 @@
-
 import sys
 from PyQt4 import QtGui, QtCore, uic
 
@@ -9,9 +8,37 @@ class MainWindow(QtGui.QMainWindow):
     def __init__(self):
         global work_dir
         
-
         QtGui.QWidget.__init__(self)
         uic.loadUi('redirect.ui', self)
+        
+        self.proc_num = 0
+
+        #Colors
+        pal = QtGui.QPalette()
+        bgc = QtGui.QColor(255, 255, 255)
+        pf_blue = QtGui.QColor(58, 135, 197)
+        pf_grey = QtGui.QColor(125, 125, 130)
+        pal.setColor(QtGui.QPalette.Base, bgc)
+        textc = QtGui.QColor(0,0,0)
+        pal.setColor(QtGui.QPalette.Text, textc)
+        self.outputbox.setPalette(pal)
+        self.outputbox_2.setPalette(pal)
+
+
+        #Font imports
+        font_instance = QtGui.QFontDatabase
+        PfFont = font_instance.addApplicationFont("TeX/font/TTF/Pfaudler-Book.ttf")
+        pf_font_family = font_instance.applicationFontFamilies(PfFont)[0]
+        pf_font = QtGui.QFont(pf_font_family)
+
+        PfFontBold = font_instance.addApplicationFont("TeX/font/TTF/Pfaudler-Bold.ttf")
+        pf_font_family_bold = font_instance.applicationFontFamilies(PfFontBold)[0]
+        pf_font_bold = QtGui.QFont(pf_font_family_bold)
+
+        self.setFont(pf_font_bold)
+        self.outputbox.setFont(pf_font)
+        self.outputbox_2.setFont(pf_font)
+
 
         #Allow copy/paste on job info line edits
         self.job_entry_1.setDragEnabled(True)
@@ -22,44 +49,51 @@ class MainWindow(QtGui.QMainWindow):
         self.job_entry_3.setReadOnly(False)
         self.job_entry_4.setReadOnly(False)
         self.job_entry_4.setReadOnly(False)
+
         
         #First LaTeX run
-        self.process = QtCore.QProcess(self)
-        self.process.readyReadStandardOutput.connect(self.stdoutReady)
-        self.process.readyReadStandardError.connect(self.stderrReady)
-        self.process.started.connect(lambda: self.clear())
-        self.process.started.connect(lambda: p('LaTeX first compile start'))
+        self.process_0 = QtCore.QProcess(self)
+        self.process_0.readyReadStandardOutput.connect(self.stdoutReady)
+        self.process_0.readyReadStandardError.connect(self.stderrReady)
+        self.process_0.started.connect(lambda: self.clear())
+        self.process_0.started.connect(lambda: p('LaTeX first compile start'))
+        self.process_0.finished.connect(lambda: self.proc_num_increase())
+
 
         #Second LaTeX run (to update table of contents)
-        self.process2 = QtCore.QProcess(self)
-        self.process2.readyReadStandardOutput.connect(self.stdoutReady2)
-        self.process2.readyReadStandardError.connect(self.stderrReady2)
-        self.process2.started.connect(lambda: p('LaTeX second compile start'))
+        self.process_1 = QtCore.QProcess(self)
+        self.process_1.readyReadStandardOutput.connect(self.stdoutReady)
+        self.process_1.readyReadStandardError.connect(self.stderrReady)
+        self.process_1.started.connect(lambda: p('LaTeX second compile start'))
+        self.process_1.finished.connect(lambda: self.proc_num_reset())
+    
+    def proc_num_increase(self):
+        self.proc_num = 1
+    def proc_num_reset(self):
+        self.proc_num = 0
 
     def clear(self):
         self.outputbox.clear()
 
     def compile_tex(self, xelatex_path, work_dir):
-        self.process.setWorkingDirectory(work_dir)
-        self.process.start(xelatex_path, ['databook'])
+        self.process_0.setWorkingDirectory(work_dir)
+        self.process_0.start(xelatex_path, ['databook'])
 
     def compile_tex2(self,xelatex_path, work_dir):
-        self.process2.setWorkingDirectory(work_dir)
-        self.process2.start(xelatex_path, ['databook'])
-    
-    def main_append(self, text):
-        #cursor = self.outputbox_2.textCursor()
-        #cursor.movePosition(cursor.End)
-        #cursor.insertText(text)
+        self.process_1.setWorkingDirectory(work_dir)
+        self.process_1.start(xelatex_path, ['databook'])
 
+    
+    #For printing to outputbox_2 (right side, for general output, see pronk function in main.py)
+    def main_append(self, text):
         cursor = self.outputbox_2.textCursor()
         cursor.movePosition(cursor.End)
         cursor.insertText(text)
         cursor.movePosition(cursor.End)
         self.outputbox_2.verticalScrollBar().setSliderPosition(self.outputbox_2.verticalScrollBar().maximum())
-
+    
+    #For printing to outputbox (left side, for latex output only)
     def append(self, text):
-        
         cursor = self.outputbox.textCursor()
         cursor.movePosition(cursor.End)
         cursor.insertText(text)
@@ -67,27 +101,18 @@ class MainWindow(QtGui.QMainWindow):
         self.outputbox.verticalScrollBar().setSliderPosition(self.outputbox.verticalScrollBar().maximum())
 
     def stdoutReady(self):
-
-        text = bytearray(self.process.readAllStandardOutput())
+        ldict = locals()
+        exec('text = bytearray(self.process_' + str(self.proc_num) + '.readAllStandardOutput())',ldict)
+        text = ldict['text']
         text = text.decode("UTF-8")
         self.append(text)
 
     def stderrReady(self):
-        text = bytearray(self.process.readAllStandardError())
+        ldict = locals()
+        exec('text = bytearray(self.process_' + str(self.proc_num) + '.readAllStandardError())',ldict)
+        text = ldict['text']
         text = text.decode("UTF-8")
         self.append(text)
-
-    def stdoutReady2(self):
-
-        text = bytearray(self.process2.readAllStandardOutput())
-        text = text.decode("UTF-8")
-        self.append(text)
-
-    def stderrReady2(self):
-        text = bytearray(self.process2.readAllStandardError())
-        text = text.decode("UTF-8")
-        self.append(text)
-
 
 
 def main():
