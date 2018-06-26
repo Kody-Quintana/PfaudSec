5e6c533b239e2d1601eed68fc067591f65c93a60
Kody.Quintana <Quintana.Kody@gmail.com>
2018-06-26 12:52:42 -0400

    Add LaTeX pgfplots templates to python file
    TeX templates for pgfplots line graphs and bar graphs added as strings
    in a list. Spaces between strings in the list are for variables that
    will be filled in by the python script.

---
+++ spreadsheet/texstorage.py
@@ -0,0 +1,93 @@
+line_graph_tex = [
+r"""
+\Large
+\begin{tikzpicture}% Line graph, use for data over time
+	\begin{axis}
+		[ymin=0,
+		%ytick distance=1,
+		title=\fontsize{25}{25}\selectfont\color{pfgrey}{
+""",
+# Title goes here
+r"""
+                },
+		tick label style={/pgf/number format/assume math mode},
+		every axis plot/.append style={ultra thick},
+		ymajorgrids,
+		bar width={0.06\textwidth},
+		legend style={
+			at={(0.5,-0.2)},
+			anchor=north,
+			legend columns=-1},
+		ylabel={},
+		symbolic x coords={
+""",
+#Symbolic x coords go here
+r"""
+			},
+		xtick=data,
+		nodes near coords,
+		nodes near coords align={vertical},
+		x tick label style={rotate=35, anchor=north east},]
+	\addplot [draw=pfblue,
+		nodes near coords={\pgfmathfloatifflags{\pgfplotspointmeta}{0}{}{\pgfmathprintnumber{\pgfplotspointmeta}}},
+		nodes near coords align={horizontal},
+		nodes near coords style={font=\Large,/pgf/number format/assume math mode}]
+		coordinates{
+""",
+#Coordinates go here
+r"""
+		};
+	\end{axis}
+\end{tikzpicture}
+"""
+]
+
+
+
+bar_graph_tex = [
+r"""
+\Large
+\begin{tikzpicture}% Bar graph, use for current month's values
+	\begin{axis}
+		[ybar,
+		ymin=0,
+		title=\fontsize{25}{25}\selectfont\color{pfgrey}{
+""",
+# Title goes here                
+r""" - \monthyeardate\today},
+		tick label style={/pgf/number format/assume math mode},
+		every axis plot/.append style={ultra thick},
+		ymajorgrids,
+		bar width={0.06\textwidth},
+		legend style={
+			at={(0.5,-0.2)},
+			anchor=north,
+			legend columns=-1},
+		ylabel={},
+		symbolic x coords={
+""",
+# symbolic x coords go here
+r"""
+			},
+		xtick=data,
+		nodes near coords,
+		nodes near coords align={vertical},
+		x tick label style={rotate=25,anchor=north east},]
+	\addplot [fill=pfblue,
+		draw=none,
+		nodes near coords={
+			\pgfmathfloatifflags
+			{\pgfplotspointmeta}{0}{}
+			{\pgfmathprintnumber{\pgfplotspointmeta}}},
+		nodes near coords align={south},
+		nodes near coords style={font=\Large,/pgf/number format/assume math mode},
+		every node near coord/.append style={xshift=0pt,yshift=-24pt,anchor=south,font=\color{white}\Large}]
+		coordinates{
+""",
+# Coordinates go here
+r"""
+		};
+	\end{axis}
+\end{tikzpicture}
+"""
+]
